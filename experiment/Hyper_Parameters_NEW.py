155acf50fc4afa6f7418b267c0ecaf28458eb101
Anne Lang <anne@annes-mbp.speedport_w_724v_typ_a_05011603_06_003>
2020-03-15 14:47:38 +0100

    test branch

--- experiment/Hyper_Parameters_NEW.py
+++ experiment/Hyper_Parameters_NEW.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import pygame
 
+###TEST 
 ######################################
 # create a data subfolder for log-data
 ######################################
