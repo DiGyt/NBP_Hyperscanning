2250e60a3d6b5c6c4b4a70187b579b943dc39c43
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2020-03-26 17:57:27 +0100

    don't break the for loop after reading the data

--- data_analysis/load_CNT/load_CNT.py
+++ data_analysis/load_CNT/load_CNT.py
@@ -40,13 +40,12 @@ if not os.path.exists(raw_files):
 #
 # iterate over raw eeg-files of each subject and save in MNE compatible .fif format
 #
-sub_list =  ['202','203','204','205','206','207','208','209','211','212']
+sub_list =  ["202"] # ['202','203','204','205','206','207','208','209','211','212']
 for i in (sub_list):
     path_to_cnt = os.path.join(path_to_cnt, "sub{}".format(i), "sub{}.cnt".format(i))
 
     # load the .cnt data with read_antcnt.py script
     raw = read_raw_antcnt(path_to_cnt, preload = False)
-    break
     # apply specific NBP channel settings
     raw.set_channel_types({ch:'misc' for ch in raw.ch_names if (ch.find('AUX')==0) | (ch.find('BIP')==0)})
     # save mne data in .fif format
