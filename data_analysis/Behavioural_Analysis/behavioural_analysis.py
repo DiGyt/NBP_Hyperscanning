37de84fb4f6b5fb23d4517aa25b268ab1d98ef5d
Anne Lang <anne@Annes-MacBook-Pro.local>
2020-04-16 16:57:12 +0200

    start with behavioural analysis script

---
+++ data_analysis/Behavioural_Analysis/behavioural_analysis.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import seaborn as sns
+sns.set()
+
+# Step1: Load and Prepare the data
+# create a list of all file names
+
+# read in all files and bind them row-wise into one df
+
+#compute real tapping-times (substract first 3s from all time points)
+
+
+def synchronize_data():
