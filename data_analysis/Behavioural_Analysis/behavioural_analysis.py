b19af327a3b62cba7d63fc68e43cb877c42afecc
Anne Lang <anne@Annes-MacBook-Pro.local>
2020-05-09 18:18:20 +0200

    continue to translate behavioural script from r to python

--- data_analysis/Behavioural_Analysis/behavioural_analysis.py
+++ data_analysis/Behavioural_Analysis/behavioural_analysis.py
@@ -1,17 +1,77 @@
 import os
+import glob
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 import seaborn as sns
-sns.set()
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 # Step1: Load and Prepare the data
 # create a list of all file names
 
 # read in all files and bind them row-wise into one df
 # copy from francas sheet!
+
+# Create path to the folder "behavioral"
+filepath = "/Users/anne/BehaviouralData"
+
+# Create a list of path names that end with .csv
+all_files = glob.glob(os.path.join(filepath, "*.csv"))
+
+# Concatenate all files to obtain a single dataframe
+df_from_each_file = (pd.read_csv(f) for f in all_files)
+df = pd.concat(df_from_each_file, ignore_index=True)
+
 #compute real tapping-times (substract first 3s from all time points)
+df['ttap3'] = df['ttap'] - 3.0
+
+
+#compute real tapping-times (substract first 3s from all time points)
+subj_list = list(df['pair'].unique())
+df['ttap']
+#for pair in subj_list:
+pair = 202
+n = len(df[df['pair'] == pair]['ttap3'])/2
+df_pair = df[df['pair'] == pair]
+sub1 = list(df_pair[df_pair['subject']== 1]['ttap3'])
+sub2 = list(df_pair[df_pair['subject']== 2]['ttap3'])
+
+list(df_pair[df_pair['subject']== 1].index)
+
+# create two dfs with sub1 and sub2
+sub1_df = df_pair.drop(list(df_pair[df_pair['subject']== 2].index), axis=0)
+sub2_df = df_pair.drop(list(df_pair[df_pair['subject']== 1].index), axis=0)
+
+sub1_df['ttap3_sub2'] = list(sub2_df['ttap3'])
+sub1_df['subject2'] = list(sub2_df['subject'])
+len(sub1_df)
+
+rejectedTrials1 = []
+rejectedTrials2 = []
+
+#loop through whole data set to check for synchrony (direction 1)
+for i in range(len(sub1)):
+    i = 1
+    min = abs(sub1[i] - sub2[i])
+    rightTap = abs(sub1[i] - sub2[i+1])
+    leftTap = abs(sub1[i] - sub2[i-1])
+    next1 = rightTap < min
+    next2 = leftTap < min
+
+    if next1 or next2:
+        rejectedTrials[count1] =
+
+for i in range(len(df[df['pair'] == pair]['ttap3'])):
+    i = 1
+    min = abs(sub1[i] - sub2[i])
+    rightTap = abs(sub1[i] - sub2[i+1])
+    leftTap = abs(sub1[i] - sub2[i-1])
+    next1 = rightTap < min
+    next2 = leftTap < min
+
+    if next1 or next2:
+        rejectedTrials[count1] =
+
 
 
 def synchronize_data():
