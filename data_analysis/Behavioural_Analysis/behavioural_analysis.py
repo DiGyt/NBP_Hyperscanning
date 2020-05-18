dad1fae1551b7f740341ea37311e97e9b0d9e6de
Anne Lang <anne@Annes-MacBook-Pro.local>
2020-05-18 17:48:54 +0200

    clean behavioural script

--- data_analysis/Behavioural_Analysis/behavioural_analysis.py
+++ data_analysis/Behavioural_Analysis/behavioural_analysis.py
@@ -6,11 +6,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # add functions script file path to sys path
-conf_path = os.getcwd()
-sys.path.append(conf_path)
-sys.path.append(conf_path + '/data_analysis')
+#conf_path = os.getcwd()
+#sys.path.append(conf_path)
+#sys.path.append(conf_path + '/data_analysis')
 
-from Behavioural_Analysis.behavioural_analysis_functions import (get_alpha, clean_data)
+#from Behavioural_Analysis.behavioural_analysis_functions import (get_alpha, clean_data)
+
 # !!!! eliminate subs 200, 210, 213, 214, 299
 #%matplotlib qt
 
@@ -29,20 +30,20 @@ all_files = glob.glob(os.path.join(filepath, "*.csv"))
 # Concatenate all files to obtain a single dataframe
 df_from_each_file = (pd.read_csv(f) for f in all_files)
 df = pd.concat(df_from_each_file, ignore_index=True)
-
 # Compute real tapping-times (substract first 3s from all time points)
 df['ttap3'] = df['ttap'] - 3.0
 subj_list = list(df['pair'].unique())
 # Compute alpha synchronization measure, individual intertap-Interval (ITI) and tapping distanca (Delta)
-df2 = get_alpha(df, subj_list)
+alpha_df = get_alpha(df, subj_list)
 
-df2[df2.pair == 206][:50]
+
+alpha_df[alpha_df.pair == 206][:50]
 # Delete all rows with "None" (all tap #9)
-df2 = df2.dropna()
+alpha_df = alpha_df.dropna()
 # Plot mean alpha synchrinization per tap (of all trials) for all pairs
 # Shows weard outliers!! --> discard in cleaning step
 
-alpha_mean_before = df2.groupby(['pair', 'tapnr'], as_index=False)['alpha_lin'].mean()
+alpha_mean_before = alpha_df.groupby(['pair', 'tapnr'], as_index=False)['alpha_lin'].mean()
 #alpha_mean.plot(kind='line',x='tapnr',y='alpha_lin', title='Alpha-average before Cleaning')
 fig, ax = plt.subplots(figsize=(8,4))
 for idx, gp in alpha_mean_before.groupby('pair'):
@@ -56,7 +57,7 @@ plt.show()
 # i.e. when the average of the eight synchrony values was higher or lower than
 # 2 s.d. from the participant’s mean synchrony (see Novembre 2012)
 
-df_cleaned = clean_data(df2)
+df_cleaned = clean_data(alpha_df)
 
 # Plot mean alpha synchrinization per tap (of all trials) for all pairs after cleaning (outliers removed)
 alpha_mean_after = df_cleaned.groupby(['pair', 'tapnr'], as_index=False)['alpha_lin'].mean()
